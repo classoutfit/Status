ffd23f4b7b8d3d6275006c425cd8e02686a7c5e2
Jeremy Burns <jeremyburns@classoutfit.com>
2017-01-07 20:07:57 +0000

    Changed how statuses are passed in.

--- View/Helper/StatusHelper.php
+++ View/Helper/StatusHelper.php
@@ -1,23 +1,25 @@
 <?php
-
 class StatusHelper extends AppHelper {
 
     var $helpers = ['Html'];
 
-    public function status($style = null, $label = null, $url = [], $domElement = 'span')
+    public function status($status = [], $url = [], $domElement = 'span')
     {
 
-        $class = !$style
+        $class = empty($status['style'])
             ? 'label'
-            : 'label label-'.$style
+            : 'label label-'.$status['style']
         ;
+
+        $label = '<i class="fa ' . $status['icon'] . '"></i>' . $status['title'];
 
         $output = '<' . $domElement . ' class="'. $class . '">';
 
         if ($url) {
             $output .= $this->Html->link(
                 $label,
-                $url
+                $url,
+                ['escape' => false]
             );
         } else {
             $output .= $label;
